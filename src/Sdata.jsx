1333eec8161e9ed5d5889ff4855075dceb78b38a
Your Name <sunitamatrix467@gmail.com>
2023-08-25 01:29:35 +0530

    first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,38 @@
+const Sdata=[
+    {
+    sname: 'DARK',    
+    imgsrc: "https://www.broadcastprome.com/wp-content/uploads/2019/08/netflix-image.jpg",
+    title: "A Netflix original series",
+    links: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmYBc1VvCcc-VCbSwB43klGzarZaq1GPUTa1WpDdIFjA&s",
+
+    },
+
+    {
+        sname: 'DARK',    
+        imgsrc: "https://www.broadcastprome.com/wp-content/uploads/2019/08/netflix-image.jpg",
+        title: "A Netflix original series",
+        links: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmYBc1VvCcc-VCbSwB43klGzarZaq1GPUTa1WpDdIFjA&s",
+
+
+    },
+
+    {
+        sname: 'DARK',    
+        imgsrc: "https://www.broadcastprome.com/wp-content/uploads/2019/08/netflix-image.jpg",
+        title: "A Netflix original series",
+        links: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmYBc1VvCcc-VCbSwB43klGzarZaq1GPUTa1WpDdIFjA&s",
+        
+
+
+    },
+
+    { sname: 'DARK',    
+    imgsrc: "https://www.broadcastprome.com/wp-content/uploads/2019/08/netflix-image.jpg",
+    title: "A Netflix original series",
+    links: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmYBc1VvCcc-VCbSwB43klGzarZaq1GPUTa1WpDdIFjA&s",
+
+    },
+
+
+]
+export default Sdata;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from "./Cards";
+import "./index.css";
+import Sdata from './Sdata'
+
+
+ReactDOM.render(
+  <>
+  <h1 className="heading_style"> List of top 5 Netflix Series in 2023 </h1>
+
+   {Sdata.map((val)=>{
+ return (
+  <Card
+ imgsrc= {val.sname}
+ title={val.title}
+ sname={val.sname}
+ links={val.links}
+ />
+);
+   })}
+
+   </>
+
+,
+document.getElementById('root')
+
+);
+
