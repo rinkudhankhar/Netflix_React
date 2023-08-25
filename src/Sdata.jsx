9179fed1fd2417dd02044bf65087f613268367bb
shreya-7869 <138999055+shreya-7869@users.noreply.github.com>
2023-08-25 14:34:00 +0530

    Update Sdata.jsx

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,13 +1,13 @@
 const Sdata=[
     {
     sname: 'DARK',    
-    imgsrc: "https://www.broadcastprome.com/wp-content/uploads/2019/08/netflix-image.jpg",
+   
     title: "A Netflix original series",
     links: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmYBc1VvCcc-VCbSwB43klGzarZaq1GPUTa1WpDdIFjA&s",
 
     },
 
-    {
+   /* {
         sname: 'DARK',    
         imgsrc: "https://www.broadcastprome.com/wp-content/uploads/2019/08/netflix-image.jpg",
         title: "A Netflix original series",
@@ -31,7 +31,7 @@ const Sdata=[
     title: "A Netflix original series",
     links: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmYBc1VvCcc-VCbSwB43klGzarZaq1GPUTa1WpDdIFjA&s",
 
-    },
+    },*/
 
 
 ]
