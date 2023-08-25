4ae4e2526cb5152389e11697a3059bc88de2a61b
shreya-7869 <138999055+shreya-7869@users.noreply.github.com>
2023-08-25 22:04:50 +0530

    Update Sdata.jsx

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,5 +1,34 @@
 const Sdata=[
+
     {
+    sname: 'DARK',    
+    imgsrc: "https://flxt.tmsimg.com/assets/p14652182_b_h9_aa.jpg",
+    title: "A Netflix original series",
+    links: "https://www.netflix.com/title/80100172",
+
+    },
+
+   {
+        sname: 'Lupin',    
+        imgsrc: "https://i.ytimg.com/vi/ga0iTWXCGa0/maxresdefault.jpg",
+        title: "A Netflix original series",
+        links: "https://www.netflix.com/title/80994082",
+
+    },
+
+    {
+        sname: 'vikings valhalla',    
+        imgsrc: "https://m.media-amazon.com/images/M/MV5BMmZlYTQ3Nj…ODk0MzA4NjQxXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg",
+        title: "A Netflix original series",
+        links: "https://www.netflix.com/title/81149450",
+        
+
+
+    },
+
+
+    
+  /*  {
     sname: 'DARK',    
     imgsrc: "https://www.broadcastprome.com/wp-content/uploads/2019/08/netflix-image.jpg",
     title: "A Netflix original series",
@@ -31,7 +60,8 @@ const Sdata=[
     title: "A Netflix original series",
     links: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmYBc1VvCcc-VCbSwB43klGzarZaq1GPUTa1WpDdIFjA&s",
 
-    },
+    },*/
+    
 
 
 ]
